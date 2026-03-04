44c956b5868c785e157edcd10e9a3450a70d5d2b
D3adlyRocket <31734254+D3adlyRocket@users.noreply.github.com>
2026-03-04 10:01:39 +0000

    Update domty.js

--- providers/domty.js
+++ providers/domty.js
@@ -1,68 +1,96 @@
+var BASE = "https://cima4u.tv";
+
 var DEFAULT_HEADERS = {
-  'User-Agent': 'Mozilla/5.0',
-  'Accept-Language': 'en-US,en;q=0.9'
+  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+  "Accept-Language": "en-US,en;q=0.9",
+  "Referer": BASE
 };
 
-function httpGet(url, headers) {
-  return fetch(url, { headers: Object.assign({}, DEFAULT_HEADERS, headers || {}) })
-    .then(r => r.text());
+function http(url, headers) {
+  return fetch(url, {
+    headers: Object.assign({}, DEFAULT_HEADERS, headers || {})
+  }).then(r => r.text());
 }
 
-// get title from tmdb
-function getTitle(tmdbId, mediaType) {
-  var type = mediaType === "movie" ? "movie" : "tv";
-  var url = "https://api.themoviedb.org/3/" + type + "/" + tmdbId + "?api_key=44c4c1f0a1d7b6f0a4c3a1e8e6e1c111";
-
-  return fetch(url).then(r => r.json()).then(j => {
-    return j.title || j.name;
-  });
-}
-
-function extractSources(html, referer) {
-  var out = [];
+function extractStreams(html, referer) {
+  var results = [];
   var re = /(https?:\/\/[^"' ]+\.(m3u8|mp4)[^"' ]*)/g;
   var m;
 
   while ((m = re.exec(html)) !== null) {
-    out.push({
-      name: "Domty",
+    results.push({
+      name: "Cima4U",
       url: m[1],
       quality: "HD",
       headers: { Referer: referer }
     });
   }
 
-  return out;
+  return results;
 }
 
-function search(base, query) {
-  return httpGet(base + "/?s=" + encodeURIComponent(query)).then(html => {
+function findMoviePage(html) {
+  var m = html.match(/<a href="(https?:\/\/[^"]+)"[^>]*class="[^"]*movie/i);
+  if (m) return m[1];
 
-    var m = html.match(/<a href="(https?:\/\/[^"]+)"[^>]*class="[^"]*title/i);
-    if (!m) return null;
+  m = html.match(/<a href="(https?:\/\/[^"]+)"/);
+  return m ? m[1] : null;
+}
 
-    return m[1];
-  });
+function findWatchPage(html) {
+  var m = html.match(/href="(https?:\/\/[^"]+watch[^"]+)"/i);
+  if (m) return m[1];
+
+  m = html.match(/data-url="(https?:\/\/[^"]+)"/i);
+  return m ? m[1] : null;
+}
+
+function getTitle(tmdbId, mediaType) {
+  var type = mediaType === "movie" ? "movie" : "tv";
+
+  var url =
+    "https://api.themoviedb.org/3/" +
+    type +
+    "/" +
+    tmdbId +
+    "?api_key=44c4c1f0a1d7b6f0a4c3a1e8e6e1c111";
+
+  return fetch(url)
+    .then(r => r.json())
+    .then(j => j.title || j.name);
 }
 
 function getStreams(tmdbId, mediaType, season, episode) {
-
   return getTitle(tmdbId, mediaType).then(title => {
 
-    var sites = [
-      "https://egybest.la",
-      "https://mycima.horse",
-      "https://cimawbas.org"
-    ];
+    var searchUrl = BASE + "/?s=" + encodeURIComponent(title);
 
-    var jobs = sites.map(site =>
-      search(site, title).then(link => {
-        if (!link) return [];
-        return httpGet(link).then(html => extractSources(html, link));
-      }).catch(() => [])
-    );
+    return http(searchUrl).then(searchHtml => {
 
-    return Promise.all(jobs).then(r => r.flat());
+      var moviePage = findMoviePage(searchHtml);
+      if (!moviePage) return [];
+
+      return http(moviePage).then(movieHtml => {
+
+        var watchPage = findWatchPage(movieHtml) || moviePage;
+
+        return http(watchPage).then(watchHtml => {
+
+          var streams = extractStreams(watchHtml, watchPage);
+
+          // also scan iframes
+          var iframeMatch = watchHtml.match(/<iframe[^>]+src="([^"]+)"/i);
+
+          if (!streams.length && iframeMatch) {
+            return http(iframeMatch[1]).then(iframeHtml =>
+              extractStreams(iframeHtml, iframeMatch[1])
+            );
+          }
+
+          return streams;
+        });
+      });
+    });
   });
 }
 
