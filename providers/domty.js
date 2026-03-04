b9eac942dcab73f68637fd83a5603b5c39810ae9
D3adlyRocket <31734254+D3adlyRocket@users.noreply.github.com>
2026-03-04 10:07:04 +0000

    Update domty.js

--- providers/domty.js
+++ providers/domty.js
@@ -1,27 +1,45 @@
-var BASE = "https://cima4u.tv";
+var BASE = "https://cimawbas.org";
 
-var DEFAULT_HEADERS = {
+var HEADERS = {
   "User-Agent": "Mozilla/5.0",
-  "Referer": BASE
+  "Accept-Language": "en,ar;q=0.9"
 };
 
-function http(url) {
-  return fetch(url, { headers: DEFAULT_HEADERS })
-    .then(function(r) { return r.text(); });
+function http(url, ref) {
+  var h = Object.assign({}, HEADERS);
+  if (ref) h["Referer"] = ref;
+
+  return fetch(url, { headers: h })
+    .then(function(r){ return r.text(); });
+}
+
+function getTitle(tmdbId, mediaType) {
+
+  var url = "https://api.themoviedb.org/3/" +
+    (mediaType === "movie" ? "movie/" : "tv/") +
+    tmdbId +
+    "?api_key=44c4ce0f5e0a5c3a7e9d";
+
+  return fetch(url)
+    .then(function(r){ return r.json(); })
+    .then(function(j){ return j.title || j.name; });
 }
 
 function extract(html, referer) {
+
   var out = [];
   var re = /(https?:\/\/[^"' ]+\.(m3u8|mp4)[^"' ]*)/g;
   var m;
 
   while ((m = re.exec(html)) !== null) {
+
     out.push({
-      name: "CIMA4U",
+      name: "DOMTY",
       url: m[1],
       quality: "HD",
       headers: { Referer: referer }
     });
+
   }
 
   return out;
@@ -29,33 +47,38 @@ function extract(html, referer) {
 
 function getStreams(tmdbId, mediaType, season, episode) {
 
-  var search = BASE + "/?s=" + encodeURIComponent(tmdbId);
+  return getTitle(tmdbId, mediaType).then(function(title){
 
-  return http(search).then(function(html) {
+    var search = BASE + "/?s=" + encodeURIComponent(title);
 
-    var match = html.match(/<a href="(https?:\/\/[^"]+)"/i);
-    if (!match) return [];
+    return http(search, BASE).then(function(html){
 
-    var page = match[1];
+      var link = html.match(/<a href="(https?:\/\/[^"]+)"/i);
+      if (!link) return [];
 
-    return http(page).then(function(p) {
+      var page = link[1];
 
-      var streams = extract(p, page);
+      return http(page, search).then(function(p){
 
-      var iframe = p.match(/<iframe[^>]+src="([^"]+)"/i);
+        var streams = extract(p, page);
 
-      if (!streams.length && iframe) {
-        return http(iframe[1]).then(function(i) {
-          return extract(i, iframe[1]);
-        });
-      }
+        var iframe = p.match(/<iframe[^>]+src="([^"]+)"/i);
 
-      return streams;
+        if (!streams.length && iframe) {
+          return http(iframe[1], page).then(function(i){
+            return extract(i, iframe[1]);
+          });
+        }
+
+        return streams;
+      });
+
     });
 
-  }).catch(function() {
+  }).catch(function(){
     return [];
   });
+
 }
 
 module.exports = { getStreams };
