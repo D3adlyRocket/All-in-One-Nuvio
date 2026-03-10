a5c18d6b788250985bfddd3fad506f9cf70d67d3
D3adlyRocket <31734254+D3adlyRocket@users.noreply.github.com>
2026-03-10 23:41:04 +0000

    Update kartoons.js

--- providers/kartoons.js
+++ providers/kartoons.js
@@ -1,52 +1,19 @@
-function getStreams(tmdbId, mediaType, seasonNum, episodeNum) {
+function getStreams() {
 
     return new Promise(function(resolve) {
 
-        try {
+        let streams = [];
 
-            let streams = [];
+        streams.push({
+            name: "Kartoons",
+            description: "HLS Stream",
+            url: "https://v11.m3u8flop.workers.dev/playlist/Wq1_siUIfZDY1dYXp_DyVRoAJTuHzXkupjbHZW_AzYpQY7Q6Vipjv82fmV7e4d14uVPIBezJ3wLXX-cfiaxGI7Wm7OPQ5wroOzMQzp-wfJxlX5sKjIrmieqdS2VR_CO7FBUOVS9SufsS9ZjsSlHCtRR17wCV5oStYS0cM4jqRAN5haEdbYfUZ1Vlc7t02lc71YG0dN2fvgOvPwzc-X6u_ulE56o7h60q_LO-",
+            behaviorHints: {
+                notWebReady: false
+            }
+        });
 
-            fetch("https://kartoons.me/player?episodeId=68b67032787cbee165d744fe")
-            .then(function(res){ return res.text(); })
-            .then(function(html){
-
-                if(!html){
-                    resolve([]);
-                    return;
-                }
-
-                let regex = /https:\/\/v\d+\.m3u8\w+\.workers\.dev\/playlist\/[A-Za-z0-9_\-]+/g;
-                let matches = html.match(regex);
-
-                if(matches){
-
-                    for(let i=0;i<matches.length;i++){
-
-                        streams.push({
-                            name: "Kartoons " + (i+1),
-                            description: "Stream",
-                            url: matches[i],
-                            behaviorHints: {
-                                notWebReady: false
-                            }
-                        });
-
-                    }
-
-                }
-
-                resolve(streams);
-
-            })
-            .catch(function(){
-                resolve([]);
-            });
-
-        } catch(e) {
-
-            resolve([]);
-
-        }
+        resolve(streams);
 
     });
 
